847df9681c4960a5a1db807bf14503f662b02fb1
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-02-28 17:38:45 +0530

    initialized new project and installed deps

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />, document.getElementById("root"));
 
@@ -11,3 +12,4 @@ root.render(<App />, document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
