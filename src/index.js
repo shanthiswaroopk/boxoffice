ad2df46c9d64a9537b1d2e83627b241cd549f553
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-02-28 18:25:07 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ root.render(<App />, document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.unregister();
+
