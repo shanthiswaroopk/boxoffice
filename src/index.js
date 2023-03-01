73656e93b4af59bb1a09415816bd8f7bd41d4f53
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-01 21:17:56 +0530

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />, document.getElementById("root"));
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>,
+ document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
