a0486fdc39a6751577836bc0c62e25b54c799b81
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-01 12:13:53 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 serviceWorkerRegistration.register();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />, document.getElementById("root"));
