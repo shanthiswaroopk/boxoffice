1c1e91bfcbee5935f423607d407a90d5be2a236f
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-03 12:25:22 +0530

    created show page components and populated them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 
@@ -48,8 +53,6 @@ useEffect( () => {
   };
 }, [id]);
 
-console.log('show', show);
-
 if(isLoading) {
   return <div>Data is being loaded</div>
 }
@@ -60,7 +63,22 @@ if(error) {
 
   return (
     <div>
-        this is show page 
+        <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres} />
+
+        <div>
+          <h2>Details</h2>
+          <Details status={show.status} network={show.network} premiered={show.premiered} />
+        </div>
+
+        <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={show._embedded.seasons} />
+        </div>
+
+        <div>
+          <h2>Cast</h2>
+          <Cast cast={show._embedded.cast} />
+        </div>
     </div>
   );
 };
