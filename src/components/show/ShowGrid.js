c3452a4c081a021bc2d5de9ddea8222efbae8bd3
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-04 07:18:50 +0530

    added starred components

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,36 +1,41 @@
 import React from 'react';
+import ShowCard from './ShowCard';
 
-import IMG_PLACEHOLDER from '../../images/not-found.png';
-import { Star } from '../styled';
-import { MainDataWrapper, Headline, TagList } from './ShowMainData.styled';
+import { FlexGrid } from '../styled';
 
-const ShowMainData = ({ name, rating, summary, tags, image }) => {
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { useShows } from '../../misc/custom-hooks';
+
+const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
-    <MainDataWrapper>
-      <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
-      <div className="text-side">
-        <Headline>
-          <h1>{name}</h1>
-          <div>
-            <Star active />
-            <span>{rating.average || 'N/A'}</span>
-          </div>
-        </Headline>
-        <div
-          className="summary"
-          dangerouslySetInnerHTML={{ __html: summary }}
-        />
+    <FlexGrid>
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
 
-        <div>
-          Tags:{' '}
-          <TagList>
-            {tags.map((tag, i) => (
-              <span key={i}>{tag}</span>
-            ))}
-          </TagList>
-        </div>
-      </div>
-    </MainDataWrapper>
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
+    </FlexGrid>
   );
 };
-export default ShowMainData;
+
+export default ShowGrid;
