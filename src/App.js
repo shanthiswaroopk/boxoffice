a2726a9156636910e5edd79aff39c0ebb51ec6ba
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-04 19:52:14 +0530

    added animation elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import Home from "./pages/Home";
 import Show from "./pages/Show";
 import Starred from "./pages/Starred";
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return(
+    <ThemeProvider theme={theme}>
   <Switch>
     <Route exact path="/">
       <Home />
@@ -24,6 +34,7 @@ function App() {
        <div>Not Found</div>
     </Route>
   </Switch>
+  </ThemeProvider>
   ); 
 }
 
