9c26eb3b07e7ff4d4ae4d9e81708ef8d4c5c3a34
shanthiswaroopk <shanthiswrupkoripalli@gmail.com>
2023-03-04 20:24:06 +0530

    Updates

---
+++ precache-manifest.7af12ee5bcb82e077fe8fa13de6aa69c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "897b9b107bb4f3c3356c",
+    "url": "/boxoffice/static/css/main.6522aa05.chunk.css"
+  },
+  {
+    "revision": "897b9b107bb4f3c3356c",
+    "url": "/boxoffice/static/js/main.897b9b10.chunk.js"
+  },
+  {
+    "revision": "19bc9adcf19c74cd26f7",
+    "url": "/boxoffice/static/js/1.19bc9adc.chunk.js"
+  },
+  {
+    "revision": "19d1e96d5e57f4e0f9fc",
+    "url": "/boxoffice/static/js/runtime~main.19d1e96d.js"
+  },
+  {
+    "revision": "50ae0da6bc5f7d402c11ed170a65a454",
+    "url": "/boxoffice/index.html"
+  }
+];
